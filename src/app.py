20b457c5f9195d8125d6327c3e488b8477b3fe3a
luke.gumbleton <luke.gumbleton@haymarket.com>
2026-01-22 10:48:22 +0000

    initial commit

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_folder="public")
+
+# Serve static files
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory(app.static_folder, path)
+
+# Serve index.html at root
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
